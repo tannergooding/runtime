2c484370fcfb2172deafec6988e971e40c5538f6
John <johnkellyoxford@gmail.com>
2020-07-08 13:42:28 +0100

    Implement equals methods

--- src/libraries/System.Numerics.Vectors/tests/MathHelper.cs
+++ src/libraries/System.Numerics.Vectors/tests/MathHelper.cs
@@ -61,15 +61,15 @@ namespace System.Numerics
             return Equal(a.X, b.X) && Equal(a.Y, b.Y);
         }
 
-        // public static bool Equal<T>(Vector3<T> a, Vector3<T> b) where T : struct
-        // {
-        //     return Equal(a.X, b.X) && Equal(a.Y, b.Y) && Equal(a.Z, b.Z);
-        // }
+        public static bool Equal<T>(Vector3<T> a, Vector3<T> b) where T : struct
+        {
+            return Equal(a.X, b.X) && Equal(a.Y, b.Y) && Equal(a.Z, b.Z);
+        }
 
-        // public static bool Equal<T>(Vector4<T> a, Vector4<T> b) where T : struct
-        // {
-        //     return Equal(a.X, b.X) && Equal(a.Y, b.Y) && Equal(a.Z, b.Z) && Equal(a.W, b.W);
-        // }
+        public static bool Equal<T>(Vector4<T> a, Vector4<T> b) where T : struct
+        {
+            return Equal(a.X, b.X) && Equal(a.Y, b.Y) && Equal(a.Z, b.Z) && Equal(a.W, b.W);
+        }
 
         public static bool Equal(Matrix4x4 a, Matrix4x4 b)
         {
